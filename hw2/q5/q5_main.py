9927920453192f50b434ed13f7e45956539305dd
OrrAvrech <orravrech@gmail.com>
2020-12-19 00:53:03 +0200

    hw2-q5 classification bonus done

--- hw2/q5/q5_main.py
+++ hw2/q5/q5_main.py
@@ -1,43 +1,85 @@
 from sklearn.datasets import load_digits
-from q5.utils import affinity_mat, diffusion_maps
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+from q5.utils import diffusion_maps, isomap, locally_linear_embedding
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.manifold import Isomap, LocallyLinearEmbedding
-from pydiffmap import diffusion_map as dm
+
+
+def generate_torus(n_samples, r, R):
+    x, y = np.random.random((2, n_samples))
+    s = np.array([(R + r * np.cos(2 * np.pi * y)) * np.cos(2 * np.pi * x),
+                  (R + r * np.cos(2 * np.pi * y)) * np.sin(2 * np.pi * x),
+                  r * np.sin(2 * np.pi * y)]).transpose()
+    return s, x, y
+
+
+def torus_color_subplots(res, s, x, y, title=''):
+    fig = plt.figure()
+    ax = fig.add_subplot(2, 2, 1, projection='3d')
+    ax.scatter(s[:, 0], s[:, 1], s[:, 2], c=res[:, 0])
+    ax.set_title('$\psi_1$')
+    ax = fig.add_subplot(2, 2, 2, projection='3d')
+    ax.scatter(s[:, 0], s[:, 1], s[:, 2], c=res[:, 1])
+    ax.set_title('$\psi_2$')
+    ax = fig.add_subplot(2, 2, 3)
+    ax.scatter(res[:, 0], res[:, 1], c=x)
+    ax.set_title('x')
+    ax = fig.add_subplot(2, 2, 4)
+    ax.scatter(res[:, 0], res[:, 1], c=y)
+    ax.set_title('y')
+    fig.suptitle(title)
+    plt.show()
+
+
+def digits_ds_subplots(ds, n_neighbors_list, title=''):
+    data, classes = ds.data, ds.target
+    fig_diff = plt.figure()
+    fig_isomap = plt.figure()
+    for i, n_neighbor in enumerate(n_neighbors_list):
+        diffusion_res = diffusion_maps(data, n_dim=2, epsilon=64, n_neighbor=n_neighbor)
+        ax = fig_diff.add_subplot(1, len(n_neighbors_list), i + 1)
+        ax.scatter(diffusion_res[:, 0], diffusion_res[:, 1], c=classes)
+        ax.set_title(f"{n_neighbor}NN")
+        fig_diff.suptitle(f"diffusion-map, {title}")
+
+        isomap_res = isomap(data, n_dim=2, n_neighbor=n_neighbor)
+        ax = fig_isomap.add_subplot(1, len(n_neighbors_list), i + 1)
+        ax.scatter(isomap_res[:, 0], isomap_res[:, 1], c=classes)
+        ax.set_title(f"{n_neighbor}NN")
+        fig_isomap.suptitle(f"isomap, {title}")
+    plt.show()
 
 
 def main():
 
+    # Section 3 #
     # 3.a
-    n_samples = 2000
-    r = 4
-    R = 10
-    x, y = np.random.random((2, n_samples))
-    s = np.array([(R + r * np.cos(2*np.pi*y)) * np.cos(2*np.pi*x),
-                  (R + r * np.cos(2*np.pi*y)) * np.sin(2*np.pi*x),
-                  r * np.sin(2*np.pi*y)]).transpose()
-    b = diffusion_maps(s, n_dim=2, epsilon=3)
-    neighbor_params = {'n_jobs': -1, 'algorithm': 'ball_tree'}
+    n_samples, r, R = (2000, 4, 10)
+    torus = generate_torus(n_samples, r, R)
+    s, x, y = torus
+    n_neighbors = [10, 50, 100]
+    for n_neighbor in n_neighbors:
+        diffusion_res = diffusion_maps(s, n_dim=2, epsilon=3, n_neighbor=n_neighbor)
+        torus_color_subplots(diffusion_res, *torus, title=f"diffusion-map, {n_neighbor}-nearest-neighbors")
+        isomap_res = isomap(s, n_dim=2, n_neighbor=n_neighbor)
+        torus_color_subplots(isomap_res, *torus, title=f"isomap, {n_neighbor}-nearest-neighbors")
+    #
+    # # 3.b.i
+    # n_classes = [3, 5, 7]
+    # digits_datasets = [load_digits(n_class=i) for i in n_classes]
+    # n_neighbors = [10, 50, 100]
+    # for i, ds in enumerate(digits_datasets):
+    #     digits_ds_subplots(ds, n_neighbors, title=f"n_class={n_classes[i]}")
 
-    # mydmap = dm.DiffusionMap.from_sklearn(n_evecs=2, k=200, epsilon='bgh', alpha=1.0, neighbor_params=neighbor_params)
-    # fit to data and return the diffusion map.
-    # a = mydmap.fit_transform(s)
-
-
-    fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.scatter(s[:, 0], s[:, 1], s[:, 2])
-    plt.scatter(b[:, 0], b[:, 1])
-
-    # 3.b
-    # digits_datasets = [load_digits(n_class=i) for i in (3, 5, 7)]
-    # a = digits_datasets[2]
-    # b = mydmap.fit_transform(a.data)
-    # b = LocallyLinearEmbedding(n_neighbors=30, n_components=2).fit_transform(a.data)
-    # b = diffusion_maps(a.data, n_dim=2, epsilon=64)
-    # plt.scatter(b[:, 0], b[:, 1], c=a.target)
-    plt.show()
+    # 3.b.ii
+    full_digits_ds = load_digits(n_class=10)
+    diffusion_embedding = diffusion_maps(full_digits_ds.data, n_dim=20, epsilon=64)
+    # isomap_embedding = isomap(full_digits_ds.data, n_dim=2, n_neighbor=10)
+    clf = SVC()
+    cv_scores = cross_val_score(clf, diffusion_embedding, full_digits_ds.target, cv=5)
+    print(cv_scores)
 
 
 if __name__ == "__main__":
--- hw2/q5/utils.py
+++ hw2/q5/utils.py
@@ -1,11 +1,7 @@
 from scipy import sparse
-from sklearn.neighbors import kneighbors_graph
+from sklearn.neighbors import NearestNeighbors, kneighbors_graph
+from sklearn.utils.graph_shortest_path import graph_shortest_path
 import numpy as np
-from sklearn.metrics.pairwise import euclidean_distances
-from scipy.linalg import eigh
-
-# class Dataset
-# add normalization by degree matrix according to some parameter alpha
 
 
 def affinity_mat(z, kernel_method='linear', epsilon=None, n_neighbor=None):
@@ -23,7 +19,7 @@ def affinity_mat(z, kernel_method='linear', epsilon=None, n_neighbor=None):
         # apply kNN
         knn_graph = kneighbors_graph(z, n_neighbors=n_neighbor)
         # turn knn-graph into a symmetric matrix
-        knn_symmetric = (knn_graph + knn_graph.transpose()).astype(bool)
+        knn_symmetric = (knn_graph + knn_graph.T).astype(bool)
         pairwise_dist = knn_symmetric.multiply(kernel)
     else:
         pairwise_dist = sparse.csr_matrix(kernel)
@@ -33,13 +29,38 @@ def affinity_mat(z, kernel_method='linear', epsilon=None, n_neighbor=None):
 
 def diffusion_maps(z, n_dim, epsilon=None, n_neighbor=None):
     adj_mat = affinity_mat(z, kernel_method='gaussian', epsilon=epsilon, n_neighbor=n_neighbor)
-    degree_mat = sparse.diags(np.array((1/adj_mat.sum(axis=0))), [0])
-    walk_mat = adj_mat.dot(degree_mat)
+    degree_mat = sparse.diags(np.array(adj_mat.sum(axis=0)), [0]).tocsc()
+    walk_mat = sparse.linalg.inv(degree_mat).dot(adj_mat)
     eigen_values, eigen_vectors = np.linalg.eigh(walk_mat.toarray())
     idx = eigen_values.argsort()[::-1]
-    eigen_values, eigen_vectors = eigen_values[idx], eigen_vectors[idx]
-    embedding = eigen_values[:n_dim] * eigen_vectors[:, :n_dim]
+    eigen_values, eigen_vectors = eigen_values[idx], eigen_vectors[:, idx]
+    eigen_values, eigen_vectors = eigen_values[1:n_dim+1], eigen_vectors[:, 1:n_dim+1]
+    embedding = np.dot(eigen_vectors, np.diag(eigen_values))
     return embedding
 
 
-# def isomap(n_neighbor=None):
+def isomap(z, n_dim, n_neighbor=None):
+    num_samples, num_features = z.shape
+    adj_mat = affinity_mat(z, n_neighbor=n_neighbor)
+    shortest_paths = graph_shortest_path(adj_mat)
+    h = np.eye(num_samples) - (1/num_samples)*np.ones((num_samples, num_samples))
+    k = -0.5 * h.dot(shortest_paths**2).dot(h)
+    eigen_values, eigen_vectors = np.linalg.eigh(k)
+    idx = eigen_values.argsort()[::-1]
+    eigen_values, eigen_vectors = eigen_values[idx], eigen_vectors[:, idx]
+    eigen_values, eigen_vectors = eigen_values[:n_dim], eigen_vectors[:, :n_dim]
+    embedding = np.dot(eigen_vectors, np.diag(eigen_values**(1/2)))
+    return embedding
+
+
+def locally_linear_embedding(z, n_dim, n_neighbor=None):
+    num_samples, num_features = z.shape
+    knn = NearestNeighbors(n_neighbors=n_neighbor).fit(z)
+    neighbors_indices = knn.kneighbors(z, return_distance=False)
+    iw = (sparse.eye(num_samples) - adj_mat)
+    b = iw.T.dot(iw)
+    eigen_values, eigen_vectors = np.linalg.eigh(b.toarray())
+    idx = eigen_values.argsort()
+    eigen_values, eigen_vectors = eigen_values[idx], eigen_vectors[:, idx]
+    embedding = eigen_vectors[:, :n_dim]
+    return embedding
